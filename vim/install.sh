49a3d46f5cdcf28b9a276d3936b7941e78a89324
Giovanni Lo Castro <giovanni@moo.com>
2017-04-17 15:32:56 +0100

    Explicit path

--- vim/install.sh
+++ vim/install.sh
@@ -1,15 +1,15 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-git clone https://github.com/morhetz/gruvbox.git
-git clone https://github.com/bling/vim-airline.git
-git clone https://github.com/ctrlpvim/ctrlp.vim.git
-git clone https://github.com/tacahiroy/ctrlp-funky.git
-git clone https://github.com/fisadev/vim-ctrlp-cmdpalette.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone git://github.com/tpope/vim-surround.git
-git clone https://github.com/artur-shaik/vim-javacomplete2.git
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/vim-airline
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp
+git clone https://github.com/tacahiroy/ctrlp-funky.git ~/.vim/bundle/ctrlp-funky
+git clone https://github.com/fisadev/vim-ctrlp-cmdpalette.git ~/.vim/bundle/vim-ctrlp-cmdpalette
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone git://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
+git clone https://github.com/artur-shaik/vim-javacomplete2.git ~/.vim/bundle/vim-javacomplete2
 
-sudo apt-get install silversearcher-ag
+apt-get install silversearcher-ag
 git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ack.vim
